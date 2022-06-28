d511617c14213a0273aa69d8ee688ed23f40501e
Marek Votava <xvotava@fi.muni.cz>
2022-06-28 17:57:26 +0200

    realtimeness

--- iteration-10-discord/frontend/src/components/EmptyPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function EmptyPage() {
-    return (
-        <div>To be continued...</div>
-    )
-}
--- iteration-10-discord/frontend/src/main.tsx
+++ iteration-10-discord/frontend/src/main.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
-import './styles/normalize.css';
-
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import './styles/normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
   </React.StrictMode>
 )
